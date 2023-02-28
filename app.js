24dafef7b031441d127c6916d47515befb7bcb32
federicomendezca <68619509+federicomendezca@users.noreply.github.com>
2023-02-28 18:33:13 -0300

    Corrección typo en app.js

--- app.js
+++ app.js
@@ -149,8 +149,8 @@ app.post("/delete", function(req, res){
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
-} 
+};
 
-app.listen(port, function>(){
+app.listen(port, function(){
     console.log("Server is up succesfully")
 });
